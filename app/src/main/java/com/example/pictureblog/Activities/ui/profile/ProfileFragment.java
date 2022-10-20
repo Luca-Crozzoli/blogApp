7bc9e730aaec2c2adc5854991abeee748ebadc10
Luca-Crozzoli <crozzoliluca9@gmail.com>
2022-10-20 10:58:16 +0200

    refactoring

--- app/src/main/java/com/example/pictureblog/Activities/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/pictureblog/Activities/ui/profile/ProfileFragment.java
@@ -16,6 +16,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.pictureblog.Activities.Home;
 import com.example.pictureblog.Adapters.PostAdapter;
 import com.example.pictureblog.Adapters.PostProfileAdapter;
 import com.example.pictureblog.Models.Post;
