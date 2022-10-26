f93b87084a2861b23c253efba67bd7e1957cd5b2
Luca-Crozzoli <crozzoliluca9@gmail.com>
2022-10-26 18:14:10 +0200

    Refactoring : enabling gps to allow the application to populate the index fo lastLocation to avoid having null last location

--- app/src/main/java/com/example/pictureblog/Activities/HomeActivity.java
+++ app/src/main/java/com/example/pictureblog/Activities/HomeActivity.java
@@ -3,16 +3,20 @@ package com.example.pictureblog.Activities;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.os.Message;
 import android.view.Gravity;
 import android.view.MenuItem;
@@ -31,8 +35,16 @@ import com.example.pictureblog.Entities.Post;
 import com.example.pictureblog.R;
 import com.example.pictureblog.databinding.ActivityHomeBinding;
 import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -64,7 +76,6 @@ import java.util.Locale;
 public class HomeActivity extends AppCompatActivity {
 
 
-
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityHomeBinding binding;
 
@@ -73,12 +84,14 @@ public class HomeActivity extends AppCompatActivity {
     private Dialog popAddPost;
 
     //popup widgets references on popup_add_post.xml
-    private ImageView popupUserImage, popupPostImage, popupMapIcon,popupAddButton;
+    private ImageView popupUserImage, popupPostImage, popupMapIcon, popupAddButton;
     private TextView popupTitle, popupPlace, popupDescription;
     private ProgressBar popupClickProgress;
     private String latitudeLongitude;
     private Uri pickedImgUri = null;
     FusedLocationProviderClient fusedLocationProviderClient;
+
+    private LocationRequest locationRequest;
 
 
     @Override
@@ -87,7 +100,7 @@ public class HomeActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         currentUser = mAuth.getCurrentUser();
-        fusedLocationProviderClient= LocationServices.getFusedLocationProviderClient( HomeActivity.this );
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient( HomeActivity.this );
 
         iniPopup();
         setupPopupImageClick();
@@ -157,14 +170,37 @@ public class HomeActivity extends AppCompatActivity {
         //initialize popup widgets
         popupUserImage = popAddPost.findViewById( R.id.popup_user_image );
         popupMapIcon = popAddPost.findViewById( R.id.map_icon_get_current_location );
+
+
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority( LocationRequest.PRIORITY_HIGH_ACCURACY );
+        locationRequest.setInterval( 5000 );
+        locationRequest.setFastestInterval( 2000 );
+
         popupMapIcon.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (ActivityCompat.checkSelfPermission( HomeActivity.this, Manifest.permission.ACCESS_FINE_LOCATION ) == PackageManager.PERMISSION_GRANTED){
-                    showLocation();
-                }else {
-                    ActivityCompat.requestPermissions( HomeActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},44 );
+
+                if (isGPSEnabled() && ActivityCompat.checkSelfPermission( HomeActivity.this, Manifest.permission.ACCESS_FINE_LOCATION ) == PackageManager.PERMISSION_GRANTED) {
+                    fusedLocationProviderClient.requestLocationUpdates( locationRequest, new LocationCallback() {
+                                @Override
+                                public void onLocationResult(LocationResult locationResult) {
+                                    super.onLocationResult( locationResult );
+
+                                    fusedLocationProviderClient.removeLocationUpdates( this );
+
+                                    if (locationResult != null && locationResult.getLocations().size() > 0) {
+                                        showLocationAddressET();
+                                    }
+                                }
+                            }, Looper.getMainLooper()
+                    );
+
+                } else {
+                    turnOnGps();
+                    ActivityCompat.requestPermissions( HomeActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44 );
                 }
+
             }
         } );
 
@@ -240,7 +276,7 @@ public class HomeActivity extends AppCompatActivity {
                             public void onSuccess(Uri uri) {
                                 String imageDownloadLink = uri.toString();
                                 // create post Object
-                                Post post = new Post( postTitle, postDescription, imageDownloadLink, currentUser.getUid(), currentUser.getPhotoUrl().toString(), latitudeLongitude,postPlace,currentUser.getDisplayName() );
+                                Post post = new Post( postTitle, postDescription, imageDownloadLink, currentUser.getUid(), currentUser.getPhotoUrl().toString(), latitudeLongitude, postPlace, currentUser.getDisplayName() );
                                 // add the post to the database
                                 addPost( post );
 
@@ -263,22 +299,75 @@ public class HomeActivity extends AppCompatActivity {
 
     }
 
+    private boolean isGPSEnabled() {
+        LocationManager locationManager = null;
+        boolean isEnable = false;
+
+        if (locationManager == null) {
+            locationManager = (LocationManager) getSystemService( Context.LOCATION_SERVICE );
+        }
+
+        isEnable = locationManager.isProviderEnabled( LocationManager.GPS_PROVIDER );
+        return isEnable;
+    }
+
+    private void turnOnGps() {
+
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest( locationRequest );
+        builder.setAlwaysShow( true );
+
+        Task<LocationSettingsResponse> result = LocationServices.getSettingsClient( getApplicationContext() )
+                .checkLocationSettings( builder.build() );
+
+        result.addOnCompleteListener( new OnCompleteListener<LocationSettingsResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
+
+                try {
+                    LocationSettingsResponse response = task.getResult( ApiException.class );
+                    Toast.makeText( HomeActivity.this, "GPS is already turned on", Toast.LENGTH_SHORT ).show();
+
+                } catch (ApiException e) {
+
+                    switch (e.getStatusCode()) {
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+
+                            try {
+                                ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+                                resolvableApiException.startResolutionForResult( HomeActivity.this, 2 );
+                            } catch (IntentSender.SendIntentException ex) {
+                                ex.printStackTrace();
+                            }
+                            break;
+
+                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                            //Device does not have location
+                            break;
+                    }
+                }
+            }
+        } );
+    }
+
     @SuppressLint("MissingPermission")
-    private void showLocation() {
+    private void showLocationAddressET() {
         fusedLocationProviderClient.getLastLocation().addOnCompleteListener( new OnCompleteListener<Location>() {
             @Override
             public void onComplete(@NonNull Task<Location> task) {
                 Location location = task.getResult();
-                if(location != null){
+                if (location != null) {
                     Geocoder geocoder = new Geocoder( HomeActivity.this, Locale.getDefault() );
                     try {
-                        List<Address> AddressList = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1  );
-                        popupPlace.setText(AddressList.get( 0 ).getAddressLine( 0 ) );
-                    }catch (IOException e){
+                        List<Address> AddressList = geocoder.getFromLocation( location.getLatitude(), location.getLongitude(), 1 );
+                        popupPlace.setText( AddressList.get( 0 ).getAddressLine( 0 ) );
+                    } catch (IOException e) {
                         e.printStackTrace();
                     }
-                }else {
-                    Toast.makeText( HomeActivity.this, "Tap again the map icon", Toast.LENGTH_SHORT ).show();
+                } else {
+                    Toast.makeText( HomeActivity.this, "Location null", Toast.LENGTH_SHORT ).show();
+                    return;
                 }
 
             }
@@ -378,4 +467,6 @@ public class HomeActivity extends AppCompatActivity {
             latitudeLongitude = address;
         }
     }
+
+
 }
