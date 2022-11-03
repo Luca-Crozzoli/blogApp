3d3dd4969d3abd70342706176bb36e969d2a45c6
Luca-Crozzoli <crozzoliluca9@gmail.com>
2022-11-03 15:38:44 +0100

    removed comment fields for image picker

--- app/src/main/java/com/example/pictureblog/Activities/HomeActivity.java
+++ app/src/main/java/com/example/pictureblog/Activities/HomeActivity.java
@@ -147,9 +147,6 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 ImagePicker.Companion.with( HomeActivity.this )
-                        /*.crop()	    			//Crop image(Optional), Check Customization for more option
-                        .compress(1024)			//Final image size will be less than 1 MB(Optional)
-                        .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)*/
                         .start();
             }
         } );
